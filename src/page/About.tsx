458ee00047fa3748b1db775a1e152b428022dbe6
jonkee <zzzgoo@gmail.com>
2023-05-23 17:33:40 +0800

    kskksak

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,21 +1,8 @@
-import {css} from "@emotion/react"
-
-
-const styles = css`
-	foo:{
-		  padding: 32px;
-      background-color: hotpink;
-      font-size: 24px;
-      border-radius: 4px;
-      &:hover {
-        color: red;
-      }
-	}
-    `
+import {container} from '../component/styles.css'
 
 export const About = ():JSX.Element => {
 	return (
-		<div css={ {styles} }>
+		<div className={container}>
 			<h1>About Page</h1>
 		</div>
 	)
